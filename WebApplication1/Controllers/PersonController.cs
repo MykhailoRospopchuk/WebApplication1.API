4e5b5e429892a414109d4258c33d1ef9185fc70c
Mykhailo <rosmisha@gmail.com>
2022-12-28 22:22:43 +0200

    Add project files.

---
+++ WebApplication1/Controllers/PersonController.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        private readonly PersonApiDbContext dbContext;
+        public PersonController(PersonApiDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+                
+        // GET: api/<PersonController>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Person>>> GetAllPersons()
+        {
+            return await dbContext.Persons.ToListAsync();
+        }
+
+        // GET api/<PersonController>/5
+        [HttpGet("{id}")] 
+        public async Task<ActionResult<Person>> GetPersonById(Guid id)
+        {
+            var person = await dbContext.Persons.FindAsync(id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(person);
+        }
+
+        // POST api/<PersonController>
+        [HttpPost]
+        public async Task<ActionResult<IEnumerable<Person>>> Post([FromBody] Person newperson)
+        {
+            if (newperson == null)
+            {
+                return NotFound();
+            }
+            else if(dbContext.Persons.Any(x => x.PhoneNumber == newperson.PhoneNumber || x.Email == newperson.Email) )
+            {
+                return BadRequest("This user is already registered");
+            }
+            else
+            {
+                newperson.Id = Guid.NewGuid();
+            }
+
+            dbContext.Persons.Add(newperson);
+            await dbContext.SaveChangesAsync();
+            return Ok(newperson);
+        }
+
+        // PUT api/<PersonController>/5
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Person>> Put(Guid id, [FromBody] Person updateperson)
+        {
+            var person = await dbContext.Persons.FindAsync(id);
+            if (updateperson == null || person == null )
+            {
+                return NotFound("User not found");
+            }
+            else
+            {
+                PropertyInfo[] fields1 = updateperson.GetType().GetProperties();
+
+                foreach (var item in fields1)
+                {
+                    var val1 = item.GetValue(person);
+                    var val2 = item.GetValue(updateperson);
+                    if (val1 != val2)
+                    {
+                        item.SetValue(person, val2);
+                    }
+                }
+            }
+            dbContext.Persons.Update(person);
+            await dbContext.SaveChangesAsync();
+            return Ok(person);
+        }
+
+        // DELETE api/<PersonController>/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Person>> Delete(Guid id)
+        {
+            var person = await dbContext.Persons.FindAsync(id);
+            if (person == null)
+            {
+                return NotFound("User not found");
+            }
+            dbContext.Persons.Remove(person);
+            await dbContext.SaveChangesAsync();
+            return Ok(person);
+        }
+    }
+}
---
+++ WebApplication1/Data/PersonApiDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class PersonApiDbContext : DbContext
+    {
+        public DbSet<Person> Persons { get; set; } = null!;
+        public PersonApiDbContext(DbContextOptions<PersonApiDbContext> options) : base(options) 
+        {
+            Database.EnsureCreated(); // create database on first using
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Person>().HasData(
+                    new Person { Id = Guid.NewGuid(), Age = 23, Name = "Anton", Surname = "Nebuybaba", Email = "antonnebuybaba@gmail.com", PhoneNumber = "+0967756482"}
+                );
+        }
+    }
+}
